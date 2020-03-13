64ed4cc70bcb350fd38068ef5faa82a7ef41bea6
TrySoehardi <trysuhardi5@gmail.com>
2020-03-14 06:19:13 +0700

    my portfolio

---
+++ js/my.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function(){
+
+    // jQuery methods go here...
+   
+  
+ 
+    if( /Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
+        // some code..
+        $('#mydiv1').prependTo( $('#MYDIV1') );
+        $('#MYDIV1').addClass('mb-3');
+        $('#mydiv2').prependTo( $('#MYDIV2') );
+        $('#MYDIV2').addClass('mb-3');
+        $('#mydiv3').prependTo( $('#MYDIV3') );
+        $('#MYDIV3').addClass('mb-3');
+       }
+       
+        
+
+    //    console.log(device);
+
+    
+
+});
